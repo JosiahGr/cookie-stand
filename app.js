dbcc6f10e0071ecc0165efd0ba5a23b18f34c3ea
Josiah Green <JosiahGreen90@gmail.com>
2018-01-15 15:32:57 -0800

    Minimum maximum chart. No total yet

---
+++ app.js
@@ -0,0 +1,122 @@
+'use strict';
+var timeOfDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// Store 1
+function mathPike() {
+  var randomNum = Math.random() * (100 - 25) + 25;
+  randomNum * 6.3;
+  randomNum = Math.floor(randomNum);
+  return (randomNum);
+}
+var salesPike = {
+  cookiesSold: [mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(), mathPike(),],
+
+  render: function() {
+    var ulEl = document.getElementById('1stPike');
+
+    for(var i = 0; i < timeOfDay.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = timeOfDay[i] + ': ' + this.cookiesSold[i] + ' Cookies Sold.';
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+salesPike.render();
+
+// Store 2
+
+function mathSeatac() {
+  var randomNum = Math.random() * (24 - 3) + 3;
+  randomNum * 1.2;
+  randomNum = Math.floor(randomNum);
+  return (randomNum);
+}
+
+var salesSeatac = {
+  cookiesSold: [mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac(), mathSeatac()],
+
+  render: function() {
+    var ulEl = document.getElementById('Seatac');
+
+    for(var i = 0; i < timeOfDay.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = timeOfDay[i] + ': ' + this.cookiesSold[i] + ' Cookies Sold.';
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+salesSeatac.render();
+
+// Store 3
+
+function mathSeattleCenter() {
+  var randomNum = Math.random() * (38 - 11) + 11;
+  randomNum * 3.7;
+  randomNum = Math.floor(randomNum);
+  return (randomNum);
+}
+
+var salesSeattleCenter = {
+  cookiesSold: [mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter(), mathSeattleCenter()],
+
+  render: function() {
+    var ulEl = document.getElementById('SeattleCenter');
+
+    for(var i = 0; i < timeOfDay.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = timeOfDay[i] + ': ' + this.cookiesSold[i] + ' Cookies Sold.';
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+salesSeattleCenter.render();
+
+
+function mathCapHill() {
+  var randomNum = Math.random() * (38 - 20) + 20;
+  randomNum * 2.3;
+  randomNum = Math.floor(randomNum);
+  return (randomNum);
+}
+
+var salesCapHill = {
+  cookiesSold: [mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill(), mathCapHill()],
+
+  render: function() {
+    var ulEl = document.getElementById('CapHill');
+
+    for(var i = 0; i < timeOfDay.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = timeOfDay[i] + ': ' + this.cookiesSold[i] + ' Cookies Sold.';
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+salesCapHill.render();
+
+
+function mathAlki() {
+  var randomNum = Math.random() * (16 - 2) + 2;
+  randomNum * 4.6;
+  randomNum = Math.floor(randomNum);
+  return (randomNum);
+}
+
+var salesAlki = {
+  cookiesSold: [mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki(), mathAlki()],
+
+  render: function() {
+    var ulEl = document.getElementById('Alki');
+
+    for(var i = 0; i < timeOfDay.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = timeOfDay[i] + ': ' + this.cookiesSold[i] + ' Cookies Sold.';
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+salesAlki.render();
