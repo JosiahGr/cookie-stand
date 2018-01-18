169ac195357923d056944616ffd0103d27f9bf11
Josiah Green <JosiahGreen90@gmail.com>
2018-01-17 16:17:38 -0800

    Added my submit event and form registers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var timeOfDay = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var cookieTable = document.getElementById('patsSalesTable');
 var totalCookies = [];
+var salesForm = document.getElementById('sales-form');
 
 function CookieStore(store, maxCust, minCust, cookieSold) {
   this.store = store;
@@ -54,6 +55,14 @@ function makeHeaderRow (head) {
   cookieTable.appendChild(trEl);
 }
 
+
+
+function renderAllStores() {
+  for(var i in totalCookies) {
+    totalCookies[i].render();
+  }
+}
+
 function makeFooterRow (foot) {
   var trEl = document.createElement('tr');
   var tdEl = document.createElement('td');
@@ -61,29 +70,43 @@ function makeFooterRow (foot) {
   trEl.appendChild(tdEl);
 
   for(var i = 0; i < timeOfDay.length; i++) {
-    totalCookies = 0;
-    tdEl = document.createElement('th');
-    tdEl.textContent = foot[i];
+    tdEl = document.createElement('td');
     trEl.appendChild(tdEl);
-  }
+  } 
   tdEl = document.createElement('td');
   tdEl.appendChild(trEl);
-  tdEl.textContent = totalHourlySales;
+  tdEl.textContent = totalCookies;
   cookieTable.appendChild(trEl);
 }
 
-var pike = new CookieStore('1st & Pike', 100, 25, 6.3);
-var seatac = new CookieStore('Seatac Airport', 24, 3, 1.2);
-var seattleCenter = new CookieStore('Seattle Center', 38, 11, 3.7);
-var capHill = new CookieStore('Capital Hill', 38, 20, 2.3);
-var alki = new CookieStore('Alki', 16, 2, 4.6);
+function addNewStore(event) {
+  event.preventDefault();
+  console.log(event.target.store.value);
+  var newStore = event.target.store.value;
+  var newMaxCust = event.target.maxCust.value;
+  var newMinCust = event.target.minCust.value;
+  var newCookieSold = event.target.cookieSold.value;
+
+  new CookieStore(newStore, newMaxCust, newMinCust, newCookieSold);
+
+  cookieTable.innerHTML = '';
+  makeHeaderRow(timeOfDay);
+  renderAllStores();
+  // makeFooterRow(totalCookies);
+
+}
+
+salesForm.addEventListener('submit', addNewStore);
+
+new CookieStore('1st & Pike', 100, 25, 6.3);
+new CookieStore('Seatac Airport', 24, 3, 1.2);
+new CookieStore('Seattle Center', 38, 11, 3.7);
+new CookieStore('Capital Hill', 38, 20, 2.3);
+new CookieStore('Alki', 16, 2, 4.6);
 
 makeHeaderRow(timeOfDay);
-pike.render();
-seatac.render();
-seattleCenter.render();
-capHill.render();
-alki.render();
-makeFooterRow(totalHourlySales);
+renderAllStores();
+makeFooterRow(totalCookies);
+
 
 
